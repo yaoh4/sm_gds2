73ed4e45c9345794fa87b1088f093afe630ec737
menons2 <menons2@nih.gov>
2016-02-16 18:21:47 +0000

    Initial checkin

---
+++ src/gov/nih/nci/cbiit/scimgmt/gds/constants/ApplicationConstants.java
@@ -0,0 +1,13 @@
+package gov.nih.nci.cbiit.scimgmt.gds.constants;
+
+/**
+ * @author menons2
+ *
+ */
+public interface ApplicationConstants {
+
+	 /*forwards */
+    public static String SUCCESS = "success";
+    public static String INPUT = "input";
+    
+}
