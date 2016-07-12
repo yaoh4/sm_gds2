dd89b9a1fb149ed4a14b3eec77faf69af4453e70
dinhys <dinhys@nih.gov>
2016-07-12 19:15:58 +0000

    Adding Page and Page Status.

--- database/INS_LOOKUP_T_DML_20160705.sql
+++ database/INS_LOOKUP_T_DML_20160705.sql
@@ -10,3 +10,23 @@ Insert into GDS.LOOKUP_T
    (ID, CODE, DISPLAY_NAME, DESCRIPTION, CREATED_DATE, CREATED_BY, DISCRIMINATOR)
  Values
    (40, 'MYSUB', 'My Project Submissions', 'My Project Submissions', sysdate, 'GDS', 'SEARCH_FROM');
+Insert into GDS.LOOKUP_T
+   (ID, CODE, DISPLAY_NAME, DESCRIPTION, CREATED_DATE, CREATED_BY, DISCRIMINATOR)
+ Values
+   (46, 'NOTSTARTED', 'Not Started', 'Not Started', sysdate, 'GDS', 'PAGE_STATUS');
+Insert into GDS.LOOKUP_T
+   (ID, CODE, DISPLAY_NAME, DESCRIPTION, CREATED_DATE, CREATED_BY, DISCRIMINATOR)
+ Values
+   (47, 'INPROGRESS', 'In Progress', 'In Progress', sysdate, 'GDS', 'PAGE_STATUS');
+Insert into GDS.LOOKUP_T
+   (ID, CODE, DISPLAY_NAME, DESCRIPTION, CREATED_DATE, CREATED_BY, DISCRIMINATOR)
+ Values
+   (48, 'GENERAL', 'General Info.', 'General Info.', sysdate, 'GDS', 'PAGE');
+Insert into GDS.LOOKUP_T
+   (ID, CODE, DISPLAY_NAME, DESCRIPTION, CREATED_DATE, CREATED_BY, DISCRIMINATOR)
+ Values
+   (49, 'BSI', 'Basic Study Info.', 'Basic Study Info.', sysdate, 'GDS', 'PAGE');
+Insert into GDS.LOOKUP_T
+   (ID, CODE, DISPLAY_NAME, DESCRIPTION, CREATED_DATE, CREATED_BY, DISCRIMINATOR)
+ Values
+   (50, 'REPOSITORY', 'Submission Status', 'Submission Status', sysdate, 'GDS', 'PAGE');
