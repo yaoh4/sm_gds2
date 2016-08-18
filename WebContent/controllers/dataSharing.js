35c6a78668e11da1f945f6d5895974e9159b6b94
dinhys <dinhys@nih.gov>
2016-08-18 15:10:56 +0000

    GDS-144: GDS Plan - Cannot delete Other Repositories

--- WebContent/controllers/dataSharing.js
+++ WebContent/controllers/dataSharing.js
@@ -32,7 +32,7 @@ $("#gds-form").on('click', '#addfield', function () {
 	{
 		// If its the second row, add a trash bin next to the first row.
 		if(fieldCount == 1) {
-			$(".otherWrapper").first().append('<span class="fa fa-trash fa-lg" title="delete" aria-hidden="true" alt="delete icon" style="font-size: 18px; padding-right: 3px;">'
+			$(".otherWrapper").first().append('<span class="fa fa-trash removeclass delete" title="Delete" aria-hidden="true" alt="delete icon" style="font-size: 18px; padding-right: 3px; margin-left: 10px; cursor:pointer">'
 					+ '</span></div>');
 		}
 		
@@ -41,7 +41,7 @@ $("#gds-form").on('click', '#addfield', function () {
 			+ '<input type="text" maxlength="200" class="other" name="otherText[25]" id="field_'
 			+ fieldCount
 			+ '" placeholder="Name of Repository" />'
-			+ '<span class="fa fa-trash removeclass delete" title="Delete"  aria-hidden="true" alt="delete icon" style="font-size: 18px; padding-right: 3px;">'
+			+ '<span class="fa fa-trash removeclass delete" title="Delete"  aria-hidden="true" alt="delete icon" style="font-size: 18px; padding-right: 3px; margin-left: 13px; cursor:pointer">'
 			+ '</span></div>');
 		
 	}
--- WebContent/jsp/content/submissionGdsPlan.jsp
+++ WebContent/jsp/content/submissionGdsPlan.jsp
@@ -158,7 +158,7 @@
 												<s:textfield id="field_%{#otherStat.index}" name="otherText[%{#ans.id}]" value="%{#other}" maxlength="200"
 													class="other" placeholder="Name of Repository" />
 												<s:if test="%{otherText[#ans.id].size > 1}">
-													<span class="fa fa-trash fa-lg" title="delete" aria-hidden="true" alt="delete" style="font-size: 18px; padding-right: 3px;"></span>
+													<span class="fa fa-trash removeclass delete" title="Delete" aria-hidden="true" alt="delete" style="font-size: 18px; padding-right: 3px; margin-left: 10px; cursor:pointer"></span>
 												</s:if>
 											</s:div>
 										</s:iterator>
