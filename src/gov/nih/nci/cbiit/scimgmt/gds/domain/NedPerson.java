9f960a922a38d148376a6b290b16642280705956
menons2 <menons2@nih.gov>
2016-10-17 14:18:10 +0000

    Add mapping to PersonRole frpm NedPerson.

--- src/gov/nih/nci/cbiit/scimgmt/gds/domain/NedPerson.java
+++ src/gov/nih/nci/cbiit/scimgmt/gds/domain/NedPerson.java
@@ -2,9 +2,13 @@ package gov.nih.nci.cbiit.scimgmt.gds.domain;
 // Generated Mar 3, 2016 3:28:59 PM by Hibernate Tools 4.0.0
 
 import java.util.Date;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -97,6 +101,8 @@ public class NedPerson implements java.io.Serializable {
 	private String nihpoc;
 	private Date inactiveDate;
 	private Date createdDate;
+	
+	private PersonRole personRole;
 
 	public NedPerson() {
 	}
@@ -909,6 +915,16 @@ public class NedPerson implements java.io.Serializable {
 
 	public void setCreatedDate(Date createdDate) {
 		this.createdDate = createdDate;
+	}
+	
+	@OneToOne
+	@JoinColumn(name="NIHSSOUSERNAME", insertable = false, updatable = false)
+	public PersonRole getPersonRole() {
+		return personRole;
+	}
+	
+	public void setPersonRole(PersonRole personRole) {
+		this.personRole = personRole;
 	}
 
 	/**
