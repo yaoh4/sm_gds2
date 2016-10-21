da68f40cb829d169559b197414de178f74199eb1
gantam2 <gantam2@nih.gov>
2016-10-21 21:32:36 +0000

    adding userRoles.

--- src/gov/nih/nci/cbiit/scimgmt/gds/domain/UserRole.java
+++ src/gov/nih/nci/cbiit/scimgmt/gds/domain/UserRole.java
@@ -21,26 +21,32 @@ import javax.persistence.Transient;
 public class UserRole implements java.io.Serializable {
 
 	private String nihNetworkId;
-	private Lookup gdsRole;
+	private String gdsRoleCode;
 	private String pdFlag;
 	private String activeFlag;
 	private String createdByFullName;
 	private String createdBy;
 	private Date createdDate;
+	private String lastChangedByFullName;
+	private String lastChangedBy; 
+	private Date lastChangedDate;
 	
 	//private NedPerson nedPerson;
 	
 	public UserRole() {
 	}
 
-	public UserRole(String nihNetworkId, Lookup gdsRole, String pdFlag, String activeFlag, String createdByFullName, String createdBy, Date createdDate) {
+	public UserRole(String nihNetworkId, String gdsRoleCode, String pdFlag, String activeFlag, String createdByFullName, String createdBy, Date createdDate, String lastChangedByFullName, String lastChangedBy , Date lastChangedDate ) {
 		this.nihNetworkId = nihNetworkId;
-		this.gdsRole = gdsRole;
+		this.gdsRoleCode = gdsRoleCode;
 		this.pdFlag = pdFlag;
 		this.activeFlag = activeFlag;
 		this.createdByFullName = createdByFullName;
 		this.createdBy = createdBy;
 		this.createdDate = createdDate;
+		this.lastChangedByFullName = lastChangedByFullName;
+		this.lastChangedBy = lastChangedBy;
+		this.lastChangedDate = lastChangedDate;
 	}
 
 	
@@ -56,12 +62,12 @@ public class UserRole implements java.io.Serializable {
 	
 	
 	@Column(name="GDS_ROLE_CODE", length = 30)
-	public Lookup getGdsRole() {
-		return gdsRole;
+	public String getGdsRoleCode() {
+		return gdsRoleCode;
 	}
 
-	public void setGdsRole(Lookup gdsRole) {
-		this.gdsRole = gdsRole;
+	public void setGdsRoleCode(String gdsRoleCode) {
+		this.gdsRoleCode = gdsRoleCode;
 	}
 
 	@Column(name="PD_FLAG", length = 4)
@@ -100,6 +106,15 @@ public class UserRole implements java.io.Serializable {
 		this.createdBy = createdBy;
 	}
 	
+	@Column(name="LAST_CHANGED_BY_FULL_NAME", length = 120)
+	public String getLastChangedByFullName() {
+		return lastChangedByFullName;
+	}
+
+	public void setLastChangedByFullName(String lastChangedByFullName) {
+		this.lastChangedByFullName = lastChangedByFullName;
+	}
+	
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "CREATED_DATE", length = 7)
 	public Date getCreatedDate() {
@@ -118,4 +133,42 @@ public class UserRole implements java.io.Serializable {
 	public void setNedPerson(NedPerson nedPerson) {
 		this.nedPerson = nedPerson;
 	}*/
+  
+	@Column(name = "LAST_CHANGED_BY", length = 120)
+	public String getLastChangedBy() {
+		return this.lastChangedBy;
+	}
+
+	public void setLastChangedBy(String lastChangedBy) {
+		this.lastChangedBy = lastChangedBy;
+	}
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "LAST_CHANGED_DATE", length = 7)
+	public Date getLastChangedDate() {
+		return lastChangedDate;
+	}
+
+	public void setLastChangedDate(Date lastChangedDate) {
+		this.lastChangedDate = lastChangedDate;
+	}
+	
+	@Transient
+	public Date getUpdatedDate() {
+		if(lastChangedDate != null) {
+			return lastChangedDate;
+		}
+		
+		return createdDate;
+	}
+	
+	@Transient
+	public String getFullName(){
+		if(lastChangedByFullName != null) {
+			return lastChangedByFullName;
+		}
+		
+		return createdByFullName;
+	}
+	
 }
