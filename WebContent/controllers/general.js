bf4fa0f64b40e0b8a57eedf5ea00d849d37f4507
nusratyj <nusratyj@nih.gov>
2016-11-17 19:35:25 +0000

    #362

--- WebContent/controllers/general.js
+++ WebContent/controllers/general.js
@@ -1,3 +1,4 @@
+//setting up default questions/grant boxes
 $(document).ready(function() {
 	$("#intramuralDiv").hide();
 	$("#nonfundedLabel").hide();
@@ -35,102 +36,5 @@ $(function(){
 		$("input[type=radio]").attr('disabled', true);
 	}
 });
-
-
-//Search/Edit button
-function openGrantsContractsSearchPage(searchType, grantContractIdPrefix) {
-	
-	$("#searchType").val(searchType);
-	$("#grantContractIdPrefix").val(grantContractIdPrefix);
-	$("#messages").empty();
-	$('#grantSearch').val('');
-	var parent = $(".tableContent").parent();
-	$(".tableContent").remove();
-	$(".tableContentOdd").remove();
-	parent.append('<tr class="tableContent"><td colspan="4">Nothing found to display.</td></tr>');
-	
-	$("#prevLinkedSubmissions").hide();
-	$("#generalInfoSection").hide();
-	$("#searchGrantsContracts").show();
-	$("#grantSearch").focus();	
-	//If user hits Enter key : 
-	$("#general_form").keydown(function( event ) {
-		if ( event.which == 13) {				
-			//Prevent default submit
-			event.preventDefault();						
-			//Hit Search
-			$( "#searchGrants" ).click();					
-		}
-	});		
-	
-}
-
-//setting up default questions/grant boxes
-
-
-
-
-//Optional Submission – Non-NIH Funded
-$("#general_form").on('click', '#submissionReason', function () {
-
-if ($("#submissionReasonId29").prop("checked", true)) {
- $("#intramuralDiv").hide();
-	$("#nonfundedLabel").show();
-	$("#extramuralHeading").hide();
-	$("#researchType").hide();
-	$("#DivisionOffice").hide();
-	$("#DpBranch").hide();
-	$("#extramuralDiv").show();
-	$("#extramuralHeading").hide();
-	
-}
-
-else {
-	$("#intramuralDiv").hide();
-	$("#nonfundedLabel").hide();
-	$("#researchType").show();
-	$("#DivisionOffice").show();
-	$("#DpBranch").show();
-	$("#extramuralDiv").show();
-	$("#extramuralHeading").show();
-	$("#extramural_grantDiv").show();
-	
-	
-
-}
-	
-});
-
-//Research Type 
-$("#general_form").on('click', '#researchType', function () {
-
-if $("#general_form_grantSelectionBoth").prop("checked", true) {
-$("#intramuralDiv").show();
-	$("#nonfundedLabel").hide();
-	$("#extramuralDiv").show();
-	$("#extramuralHeading").show();
-	$("#nonfundedLabel").hide();
-}
-
-if $("#general_form_grantSelectionIntramural").prop("checked", true) {
-$("#intramuralDiv").show();
-	$("#nonfundedLabel").hide();
-	$("#extramuralDiv").hide();
-	$("#extramuralHeading").hide();
-	$("#nonfundedLabel").hide();
-}
-
-else {
-	$("#intramuralDiv").hide();
-	$("#nonfundedLabel").hide();
-	$("#extramuralDiv").show();
-	$("#extramuralHeading").show();
-	$("#nonfundedLabel").hide();
-
-}
-	
-});
-
-
 
 
