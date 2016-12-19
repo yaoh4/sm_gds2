b8ff3a2c69a92282fcd5f707c96a64cb7fece8ec
nusratyj <nusratyj@nih.gov>
2016-12-19 21:08:51 +0000

    fix to tooltip link issue

--- WebContent/scripts/theme.js
+++ WebContent/scripts/theme.js
@@ -118,27 +118,7 @@ $('#submissionTable').dataTable( {
   "autoWidth": false
 } );
 
-$(function () {
-    $(document).tooltip({
-        content: function () {
-            return $(this).prop('title');
-        },
-        show: null, 
-        close: function (event, ui) {
-            ui.tooltip.hover(
 
-            function () {
-                $(this).stop(true).fadeTo(400, 1);
-            },
-
-            function () {
-                $(this).fadeOut("400", function () {
-                    $(this).remove();
-                })
-            });
-        }
-    });
-});
 
 
 
